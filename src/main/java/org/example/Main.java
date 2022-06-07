3712fd2d3f4bb98438c2c99db2b32b1962bc0db1
kajela <titoyut56@gmail.com>
2022-06-07 01:45:35 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
+package org.example;
+
+ public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        int y = 10;
+        do {
+            System.out.println(y);
+            y++;
+        }
+            while (y < 16);
+            for (int z= 0;z < 6; z = z+2) {
+                System.out.println(z);
+            }
+
+        }
     }
-}
